32a751eb94672a2a4b8a588dc758ac8d64f7cb37
George Moore <gdubya1218@gmail.com>
2020-01-23 13:36:54 -0700

    Test

--- JPAHealth/src/main/java/com/skilldistillery/HealthApp/entities/User.java
+++ JPAHealth/src/main/java/com/skilldistillery/HealthApp/entities/User.java
@@ -1,5 +1,62 @@
 package com.skilldistillery.HealthApp.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String username;
+	private String password;
+	private Boolean active;
+	private String role;
+	
+	
+	public User() {
+		super();
+	}
+	public User(int id, String username, String password, Boolean active, String role) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.active = active;
+		this.role = role;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Boolean getActive() {
+		return active;
+	}
+	public void setActive(Boolean active) {
+		this.active = active;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
 
 }
--- JPAHealth/src/test/java/com/skilldistillery/HealthApp/entities/UserTest.java
+++ JPAHealth/src/test/java/com/skilldistillery/HealthApp/entities/UserTest.java
@@ -1,6 +1,10 @@
 package com.skilldistillery.HealthApp.entities;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
@@ -10,25 +14,36 @@ import org.junit.jupiter.api.Test;
 
 class UserTest {
 
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private User user;
+	
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		 emf = Persistence.createEntityManagerFactory("HealthDb");
+
 	}
 
 	@AfterAll
 	static void tearDownAfterClass() throws Exception {
+		 emf.close();
+
 	}
 
 	@BeforeEach
 	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		user = em.find(User.class, 1);
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
+		em.close();
 	}
 
 	@Test
 	void test() {
-		fail("Not yet implemented");
+		assertEquals("admin", user.getUsername());
 	}
 
 }
