e401dd54a1c32cf9e5c056a91c08fd6714b4761f
Josh-Hollopeter <joshuahollopeter@gmail.com>
2020-01-24 09:42:33 -0700

    updated user entity

--- JPAHealth/src/main/java/com/skilldistillery/HealthApp/entities/User.java
+++ JPAHealth/src/main/java/com/skilldistillery/HealthApp/entities/User.java
@@ -26,12 +26,10 @@ public class User {
 	private String lastName;
 	private Boolean active;
 	private String role;
-	private Integer age;
 	private String email;
 	private String gender;
 	
-	@Column(name = "address_id")
-	private Integer addressId;
+
 	
 //	@ManyToMany
 //	@JoinTable(name="user_workout",
@@ -81,5 +79,32 @@ public class User {
 	public void setRole(String role) {
 		this.role = role;
 	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	
 
 }
