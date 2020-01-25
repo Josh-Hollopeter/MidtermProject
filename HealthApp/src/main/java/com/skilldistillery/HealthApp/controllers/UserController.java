a295597780276b55733bb5051a05608b7b6c7169
George Moore <gdubya1218@gmail.com>
2020-01-24 17:08:07 -0700

    Login sessionScope addded to userhome jsp

--- HealthApp/src/main/java/com/skilldistillery/HealthApp/controllers/UserController.java
+++ HealthApp/src/main/java/com/skilldistillery/HealthApp/controllers/UserController.java
@@ -2,6 +2,7 @@ package com.skilldistillery.HealthApp.controllers;
 
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,14 +14,15 @@ import com.skilldistillery.HealthApp.entities.User;
 @Controller
 public class UserController {
 	
-	HealthAppDAOImpl dao = new HealthAppDAOImpl();
+	@Autowired
+	HealthAppDAOImpl dao;
 	
 	@RequestMapping(path = {"/", "index.do"} )
 	public String home(Model model, HttpSession session, User user) {
 		
 //		User user = (User)session.getAttribute("user");
 		
-		session.setAttribute("user", user);
+//		session.setAttribute("user", user);
 		
 		return "index";
 	}
@@ -34,23 +36,25 @@ public class UserController {
 //	}
 	
 	@RequestMapping(path = {"login.do"})
-	public String loginPostView(@RequestParam("username") String username, @RequestParam("password") String password, HttpSession session, Model model, User user) {
-		
-		if (session.getAttribute("user") != null) {
-			return "userhome";
-		}
-			user = null;
-		try {
-			user = dao.findByLogin(username, password);
-		}catch(Exception e) {
-			
-		}
+	public String loginPostView( HttpSession session, User user,Model model) {
+		System.out.println(user);
+//		user = null;
+//		try {
+			user = dao.findByLogin(user.getUsername(), user.getPassword());
+			System.out.println(user);
+//			user = dao.findByLogin(user.getUsername(), user.getPassword());
+//		}catch(Exception e) {
+//			
+//		}
+	
 		
 		if ( user == null) {
 			model.addAttribute("error", "No user Found");
 			return "index";
-		}
+		}else {
 		session.setAttribute("user", user);
+		}
+		
 		return "userhome";
 		
 	}
